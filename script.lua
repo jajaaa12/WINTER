34536f4521be5393dfd1adf3420c097210f9bbf8
jajaaa12 <feson32151@gmail.com>
2026-03-19 23:42:21 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -149,7 +149,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Location Rods"] = {
             ["Fisherman Island"] = {"Starter Rod"},
             ["Tropical Grove"] = {"Grass Rod", "Midnight Rod"},
-            ["Treasure Room"] = {"Ares Rod", "Astral Rod", "Element Rod", "Ghostfinn Rod"},
+            ["Treasure Room"] = {"Ares Rod", "Astral Rod"},
             ["Ancient Ruin"] = {"Element Rod", "Ghostfinn Rod"}
         },
         ["Endgame"] = "Ancient Ruin",
